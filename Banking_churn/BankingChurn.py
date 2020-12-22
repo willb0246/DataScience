f7c150ac8cf96cb8ff5e51abd88e2990d56d5ef1
William Burke <williamtburke311@gmail.com>
2020-12-21 21:18:46 -0600

    first project, trying to identify what customer features contribute to high churn

---
+++ Banking_churn/BankingChurn.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+plt.close('all')
+
+#pd.set_option('display.max_columns', None)
+
+ds = pd.read_csv('BankChurners.csv')
+
+
+
