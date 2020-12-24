e79ab96c05ebf6b68076d84a33e3f76491a32cd4
William Burke <williamtburke311@gmail.com>
2020-12-24 17:36:11 -0500

    Pickling loaded the model! v2

--- Statistical Tests/running_model.py
+++ Statistical Tests/running_model.py
@@ -5,4 +5,11 @@ model = pickle.load(open('Pickle_ML_File.pkl', 'rb'))
 print(model.support_)
 print(model.ranking_)
 
+cols=['euribor3m', 'job_blue-collar', 'job_housemaid', 'marital_unknown', 'education_illiterate', 'default_no', 'default_unknown',
+      'contact_cellular', 'contact_telephone', 'month_apr', 'month_aug', 'month_dec', 'month_jul', 'month_jun', 'month_mar',
+      'month_may', 'month_nov', 'month_oct', "poutcome_failure", "poutcome_success"]
+X=os_data_X[cols]
+y=os_data_y['y']
+
+
 #print(model)
