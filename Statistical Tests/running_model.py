6c146d3cfc5f043f2518ad2e7567fb7d9fdc24a4
William Burke <williamtburke311@gmail.com>
2020-12-27 19:59:48 -0600

    ROC curve to assess the accuracy of the model. (0.91 AUC)

--- Statistical Tests/running_model.py
+++ Statistical Tests/running_model.py
@@ -78,4 +78,21 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 logreg = LogisticRegression()
 yyss = logreg.fit(X_train, y_train)
 y_pred = logreg.predict(X_test)
-print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
+#print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
+
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+logit_roc_auc = roc_auc_score(y_test, logreg.predict(X_test))
+fpr, tpr, thresholds = roc_curve(y_test, logreg.predict_proba(X_test)[:,1])
+plt.figure()
+plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.legend(loc="lower right")
+plt.savefig('Log_ROC')
+plt.show()
+
