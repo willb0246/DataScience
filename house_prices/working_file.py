2cd852328b3fe8d98493f60263f2ceca141dd806
William Burke <williamtburke311@gmail.com>
2020-12-29 15:36:19 -0600

    Adding initial csv files for this project

---
+++ house_prices/working_file.py
@@ -0,0 +1,11 @@
+# load all packages
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import norm
+from sklearn.preprocessing import StandardScaler
+from scipy import stats
+import warnings
+warnings.filterwarnings('ignore')
+
