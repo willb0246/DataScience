597eb1d3ea746cb800157141f8c176eaf224a859
William Burke <williamtburke311@gmail.com>
2020-12-29 21:25:16 -0600

    working on linear regression for sale price

--- house_prices/working_file.py
+++ house_prices/working_file.py
@@ -7,7 +7,10 @@ from scipy.stats import norm
 from sklearn.preprocessing import StandardScaler
 from scipy import stats
 import warnings
+from sklearn.linear_model import LinearRegression
+from sklearn.feature_selection import RFE
 warnings.filterwarnings('ignore')
+
 
 '''
 # here is the columns output
@@ -35,16 +38,48 @@ df_train = pd.read_csv('initial_train.csv')
 df_train.dropna()
 pd.set_option('display.max_columns', None)
 
+'''
+# correlation heat map
+corrmat = df_train.corr()
+f, ax = plt.subplots(figsize=(12, 9))
+sns.heatmap(corrmat, vmax=0.8, square=True)
+'''
+
 # this script outputs a list of int64 columns, then runs for loop. For every var in list it correlates the var with SalePrice
 # then it prints any correlations that have an absolute value over 0.3
 y = list(df_train.select_dtypes(include=['int64']))
+yy = []
+correlated_vars = []
 for i in y:
-    int_var = df_train[i]
-    corre = int_var.corr(df_train['SalePrice'], method='pearson')
-    if abs(corre) > 0.3:
-        print(i, corre)
-    else:
+    if i == 'SalePrice':
         pass
+    else:
+        yy.append(i)
+        int_var = df_train[i]
+        corre = int_var.corr(df_train['SalePrice'], method='pearson')
+        if abs(corre) > 0.3:
+            correlated_vars.append(i)
+        else:
+            pass
 
-#sns.distplot(df_train['YearBuilt'])
+columns = []
+df_train = pd.get_dummies(df_train)
+df_train = df_train.dropna()
+for i in df_train.columns:
+    if i == 'SalePrice':
+        pass
+    else:
+        columns.append(i)
+X = df_train[columns]
+y = df_train['SalePrice']
+
+# find the NaN
+NaNFinder = np.where(np.isnan(df_train))
+
+model = LinearRegression()
+rfe = RFE(model, 8)
+rfe = rfe.fit(X, y)
+print('Selected features: %s' % list(X.columns[rfe.support_]))
 #plt.show()
+
+# Selected features: ['OverallQual', 'YearBuilt', 'YearRemodAdd', '1stFlrSF', '2ndFlrSF', 'FullBath', 'Fireplaces', 'GarageCars']
